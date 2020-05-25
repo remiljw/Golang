ab649712dd598cc97662359054ce4f490d845e4f
Michael Ajanaku <remiljw@gmail.com>
2020-05-25 19:02:49 +0100

    Multiple slice

--- Complex Types/slices.go
+++ Complex Types/slices.go
@@ -1,11 +1,13 @@
 package main
+
 import (
 	"fmt"
 	"os"
 )
+
 // takes an int argument and returns a string slice
-func getPassedArgs(minArgs int) []string{
-	if len(os.Args) < minArgs{
+func getPassedArgs(minArgs int) []string {
+	if len(os.Args) < minArgs {
 		fmt.Printf("At least %v arguments are needed\n", minArgs)
 		os.Exit(1)
 	}
@@ -15,9 +17,10 @@ func getPassedArgs(minArgs int) []string{
 	}
 	return args
 }
+
 // loops over a slice and finds the longest string.
 func findLongest(args []string) string {
-	var longest string 
+	var longest string
 	for i := 0; i < len(args); i++ {
 		if len(args[i]) > len(longest) {
 			longest = args[i]
@@ -25,11 +28,33 @@ func findLongest(args []string) string {
 	}
 	return longest
 }
-func main(){
+
+// Appending Multiple Items to a Slice\\
+// function to grab user input
+func getPassArgs() []string {
+	var args []string
+	for i := 1; i < len(os.Args); i++ {
+		args = append(args, os.Args[i])
+	}
+	return args
+}
+
+// function that accepts a slice of strings as parameters and returns a slice of strings
+func getLocals(extraLocals []string) []string {
+	var locales []string
+	locales = append(locales, "en_US", "fr_FR")
+	locales = append(locales, extraLocals...)
+	return locales
+}
+func main() {
+	// must run inputting @least 3 string argument e.g go run . how are you
 	if longest := findLongest(getPassedArgs(3)); len(longest) > 0 {
 		fmt.Println("The longest word passed was:", longest)
 	} else {
 		fmt.Println("There was an error")
 		os.Exit(1)
 	}
+	// Appending Multiple items to a slice
+	locales := getLocals(getPassArgs())
+	fmt.Println("Locales to use:", locales)
 }
