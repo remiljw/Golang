c72c0f3c41fea49bf0c15da9cef4096d4bf791ea
Michael Ajanaku <remiljw@gmail.com>
2020-05-25 19:15:33 +0100

    create_slice

---
+++ Complex Types/create_slice.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+// Creating Slices from a Slice
+func message() string{
+	s := []int{1,2,3,4,5,6,7,8,9}
+	// extract the first value
+	m := fmt.Sprintln("First:", s[0], s[0:1], s[:1])
+	// extract the last element
+	fmt.Sprintln("Last :", s[len(s)-1], s[len(s)-1:len(s)], s[len(s)-1:])
+	// get the first five values and add to the message
+	m += fmt.Sprintln("First 5:", s[:5])
+	// get the last four values
+	m += fmt.Sprintln("Last 4:". s[5:])
+	// get 5 values from  the middle
+	m += fmt.Sprintln("Middle 5:", s[2:7])
+	return m
+}
+func main(){
+	fmt.Print(message())
+}
