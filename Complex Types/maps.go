a236f4dffd6f58f1cc6273bc1530c343449d7c5f
Michael Ajanaku <remiljw@gmail.com>
2020-05-27 12:19:12 +0100

    Reading from a map

--- Complex Types/maps.go
+++ Complex Types/maps.go
@@ -1,6 +1,9 @@
 // Creating, Reading and Writing a Map
 package main
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func getUsers() map[string] string{
 	users := map[string]string{
@@ -11,7 +14,26 @@ func getUsers() map[string] string{
 	users["073"] = "Tracy"
 	return users
 }
+func getUser(id string) (string,bool){
+	users := getUsers()
+	user, exists := users[id]
+	return user, exists
+}
 
 func main(){
-	fmt.Println("Users:", getUsers())
+	if len(os.Args) < 2{
+		fmt.Println("User ID not passed")
+		os.Exit(1)
+	}
+	userID := os.Args[1]
+	name, exists := getUser(userID)
+	if !exists {
+		fmt.Printf("Passed user ID (%v) not found. \nUsers:\n", userID)
+		for key, value := range getUsers() {
+			fmt.Println(" ID:", key, "Name:", value)
+		}
+		os.Exit(1)
+	}
+	fmt.Println("Name:", name)
+	// fmt.Println("Users:", getUsers())
 }
