da7972134cbb5aa45bba1f0001097bc39db89215
Michael Ajanaku <remiljw@gmail.com>
2020-05-28 17:15:29 +0100

    asserting

--- Complex Types/type_assertion.go
+++ Complex Types/type_assertion.go
@@ -4,19 +4,60 @@ import (
 	"fmt"
 )
 func doubler(v interface{}) (string, error) {
-	if i, ok := v.(int); ok{
-		return fmt.Sprint(i * 2), nil
+	switch t := v.(type) {
+	case string:
+		return t + t, nil
+	case bool:
+		if t{
+			return "truetrue", nil
+		}
+		return "falsefalse", nil
+	case float32, float64:
+		if f, ok := t.(float64); ok{
+			return fmt.Sprint(f * 2), nil
+		}
+		return fmt.Sprint(t.(float32) * 2), nil
+	case int: 
+		return fmt.Sprint(t * 2), nil 
+	case int8: 
+		return fmt.Sprint(t * 2), nil 
+	case int16: 
+		return fmt.Sprint(t * 2), nil 
+	case int32: 
+		return fmt.Sprint(t * 2), nil 
+	case int64: 
+		return fmt.Sprint(t * 2), nil 
+	case uint: 
+		return fmt.Sprint(t * 2), nil 
+	case uint8: 
+		return fmt.Sprint(t * 2), nil 
+	case uint16: 
+		return fmt.Sprint(t * 2), nil 
+	case uint32: 
+		return fmt.Sprint(t * 2), nil 
+	case uint64: 
+		return fmt.Sprint(t * 2), nil 
+	default:
+		return "", errors.New("unsupported type passed")
 	}
-	if s, ok := v.(string); ok{
-		return s + s, nil
-	}
-	return "", errors.New("unsupported type passed")
+	// if i, ok := v.(int); ok{
+	// 	return fmt.Sprint(i * 2), nil
+	// }
+	// if s, ok := v.(string); ok{
+	// 	return s + s, nil
+	// }
+	// return "", errors.New("unsupported type passed")
 }
 func main() {
-	res,_ := doubler(5)
+	res,_ := doubler(-5)
+	fmt.Println("-5:", res)
+	res,_ = doubler("5")
 	fmt.Println("5:", res)
 	res,_ = doubler("yum")
 	fmt.Println("yum:", res)
-	_, err := doubler(true)
-	fmt.Println("true:", err)
+	res,_ = doubler(true)
+	fmt.Println("true:", res)
+	res,_ = doubler(float32(3.14))
+	fmt.Println("3.14:", res)
+
 }
